9cd44233550f38174db3e626220a54893efc2441
satoshi-34 <satoshi21365@gmail.com>
2023-11-18 11:17:18 +0900

    Add 初期設定&基礎実装

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/posts',[PostController::class,'index']);
+/*
+/postsというuriにgetメソッドで注文が来たらpostcontrollerのindexを実行
+*/

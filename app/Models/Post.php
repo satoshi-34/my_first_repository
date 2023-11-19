e240e59d8ca98eb9a00b8a4cb22eb1ab8121f26d
satoshi-34 <satoshi21365@gmail.com>
2023-11-19 10:20:54 +0900

    pagination

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    public function getPaginateByLimit(int $limit_count = 5)
+    {
+         // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at','DESC')->paginate($limit_count);
+        //更新日を光順で並び替えて上から十件のデータ取得
+    }
 }
